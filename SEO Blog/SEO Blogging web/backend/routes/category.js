679254a2413e802870c67e4ce46e8377206b04e1
Deepak Singh Maurya <mauryagdeepak@gmail.com>
2020-11-02 11:24:03 +0530

    added code for category

--- SEO Blog/SEO Blogging web/backend/routes/category.js
+++ SEO Blog/SEO Blogging web/backend/routes/category.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { create, list, read, remove } = require('../controllers/category');
 
-// validators
+// validators code
 const { runValidation } = require('../validators');
 const { categoryCreateValidator } = require('../validators/category');
 const { requireSignin, adminMiddleware } = require('../controllers/auth');
@@ -11,5 +11,4 @@ router.post('/category', categoryCreateValidator, runValidation, requireSignin, 
 router.get('/categories', list);
 router.get('/category/:slug', read);
 router.delete('/category/:slug', requireSignin, adminMiddleware, remove);
-
 module.exports = router;

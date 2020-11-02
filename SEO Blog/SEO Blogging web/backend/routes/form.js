04b18b667aec7412a634c80906110aa88a4d9845
Deepak Singh Maurya <mauryagdeepak@gmail.com>
2020-11-02 11:25:18 +0530

    validators code for contact form

--- SEO Blog/SEO Blogging web/backend/routes/form.js
+++ SEO Blog/SEO Blogging web/backend/routes/form.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { contactForm, contactBlogAuthorForm } = require('../controllers/form');
 
-// validators
+// validators code for contact form
 const { runValidation } = require('../validators');
 const { contactFormValidator } = require('../validators/form');
 
